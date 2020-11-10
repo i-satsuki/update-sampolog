62948cc573694f2934de43ddafa74e8b302033fd
vagrant <vagrant@localhost.localdomain>
2020-11-10 13:57:35 +0000

    [Add]admin-search

---
+++ app/controllers/admin/search_controller.rb
@@ -0,0 +1,26 @@
+class Admin::SearchController < ApplicationController
+  before_action :authenticate_admin!
+
+  def search
+    @content = params["search"]["content"]
+    @model = params["search"]["model"]
+    @method = params["search"]["method"]
+    @records = search_for(@content, @model, @method)
+  end
+
+  private
+
+  def search_for(content, model, method)
+    if model == 'user'
+      if method == 'perfect'
+        User.where(name: content )
+      elsif  method == 'forward'
+        User.where('name LIKE ?' , content+'%' )
+      elsif  method == 'backward'
+        User.where('name LIKE ?' ,'%'+content )
+      else
+        User.where('name LIKE ?' , '%'+content+'%' )
+      end
+    end
+  end
+end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,4 @@
-class Admins::UsersController < ApplicationController
+class Admin::UsersController < ApplicationController
 	before_action :authenticate_admin!
 
 	def index
